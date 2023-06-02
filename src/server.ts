96787050b94988784b3ad7da95d21a1dd8d14448
Yusuke Wada <yusuke@cloudflare.com>
2023-06-02 17:15:03 +0900

    bump up

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { Hono } from 'hono'
-import { serveStatic } from 'hono/serve-static.module'
+import { serveStatic } from 'hono/cloudflare-workers'
 import App from './App'
 import api from './api'
 import { ssr } from './ssr/middleware'
 
 const app = new Hono()
+
 app.route('/api', api)
 app.get('/assets/*', serveStatic({ root: './public' }))
 
